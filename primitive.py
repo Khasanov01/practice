b7d2aa3855d9b6e0516d3d6c3583f2275da5f55f
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-17 14:00:57 +0500

    feat:learn string in python

--- primitive.py
+++ primitive.py
@@ -10,3 +10,19 @@ print("=============")
 result1 = count.bit_count() #method
 result2 = count.numerator #state
 print(result1, result2)
+
+print("========= tsring ========")
+#methods: upper(), lower(), title(), find(), replace()
+course = "AI python Fullstack"
+result = type(course)
+print(f"type of course(1): {result}")
+
+result=course.title()
+print(f"result(2):{result}")
+
+result=course.upper()
+print(f"the result(3):{result}")
+
+result=course.replace("AI", "artificial intelligance")
+print(f"the result(4):{result}")
+print(course)
