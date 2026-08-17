f7e9a6b29ade4e76e49dbbf7fedf8f81b73f23bd
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-17 12:11:47 +0500

    feat:python system variables and special methods via dunders

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 print("hello world")
+message = "python; everythng is object"
+print(message)
+
+result = type(message)
+print("result:", result)
+'''
+in python there are builtin tools>
+(1) Types:int float str list
+(2) Functions: print() str() len() input() type()
+(3) Constants: true false none
+'''
+print(dir(__builtins__))
