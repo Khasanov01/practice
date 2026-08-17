e9b3adf239871cb50f620d7998d42728349da343
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-17 21:31:02 +0500

    feat:specific error handling system

--- objects.py
+++ objects.py
@@ -5,23 +5,41 @@
 (4) error handling system
 '''
 
-import array #package or module
-import math #package
+import array  # package or module
+import math  # package
 from math import ceil, asin
 
 print("====== what is an object ======")
-#an object has state and method properties
-#everything is object in python
+# an object has state and method properties
+# everything is object in python
 print(type("Hello World"))
 print(type(100))
 print(type(True))
 print(type(array))
 print(type(math))
 
-#Paradigm> OOP functional & Proggramming
-#OOP 4 concepts> Abstruction Encapsulation Inheritance polimorphism 
-result1 = math.ceil(97.7) #CALL
+# Paradigm> OOP functional & Proggramming
+# OOP 4 concepts> Abstruction Encapsulation Inheritance polimorphism
+result1 = math.ceil(97.7)  # CALL
 print(result1)
 
-result2=ceil(98.7)
+result2 = ceil(98.7)
 print(result2)
+
+
+print("========= error handling system =========")
+car_dict = dict(name="Toyota", year=2006, electric=True)
+
+try:
+    print("passed here")
+    a = car_dict.speed
+    result = car_dict["origin"]
+    print(result)
+except KeyError as err:
+    print('no origin state property found:', err)
+except AttributeError as err:
+    print('no speed found:', err)
+else:
+    print("Logic executed successfully without errors")
+finally:
+    print("final closing logic")
