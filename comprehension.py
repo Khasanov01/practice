95370cb350bbfd552feda05cfc7445cab98deed1
khasanov01 <xasanovnurilloh07@gmail.com>
2026-08-21 12:01:20 +0500

    feat:learn set and dictionary comprehensions

--- comprehension.py
+++ comprehension.py
@@ -3,22 +3,22 @@
 (2)set and dictionary comp.
 '''
 print("======== what is comprehension & list comp. =======")
-#comprehension acts like spread operator!
+# comprehension acts like spread operator!
 
 '''Comprehension general syntax:
 a) *iterable
 b) <expression> for item in iterate
 c) <expression> for item in iterate <condition>
 '''
-#list comp.
-numbers=[1, 2, 4, 2,  1, 20]
-list_numbers=[*numbers] # a version
+# list comp.
+numbers = [1, 2, 4, 2,  1, 20]
+list_numbers = [*numbers]  # a version
 print('list_numbers:', list_numbers)
 print(numbers is list_numbers)
 
 print("-------")
-people = [("Robert", 20), ("Steve", 19), ("Joseph", 25)]
-list_people = [person[0] for person in people] # b version
+people = [("Robert", 21), ("Steve", 19), ("Joseph", 25)]
+list_people = [person[0] for person in people]  # b version
 print("list_people:", list_people)
 
 print("-------")
@@ -29,5 +29,18 @@ cars = [
     ("BMW", 109),
     ("Pagani", 33)
 ]
-list_cars = [car[0] for car in cars if car[1] > 80] # c version
+list_cars = [car[0] for car in cars if car[1] > 80]  # c version
 print("list_cars:", list_cars)
+
+print("======== Set and Dictionary Comprehension =======")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+
+set_numbs = {*numbs}
+print("set_numbs:", set_numbs)
+
+dict_people = {person[0]: person[1] for person in people}  # b version
+print('dict_people', dict_people)
+
+dict_people2 = {person[0]: person[1]
+               for person in people if person[1] > 20}  # c version
+print('dict_people2', dict_people2)
